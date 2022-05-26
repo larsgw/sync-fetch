c65b3aa4c392e1c34af7957dd86d66ef0700a29d
Lars Willighagen <lars.willighagen@gmail.com>
2022-05-26 22:13:24 +0200

    fix(browser): improve browser compatibility

--- browser.js
+++ browser.js
@@ -3,14 +3,13 @@
 const { Buffer } = require('buffer/')
 
 function syncFetch (...args) {
-  const [url, opts] = parseArgs(...args)
+  const request = new syncFetch.Request(...args)
 
   const xhr = new XMLHttpRequest()
-  xhr.withCredentials = opts.credentials === 'include'
-  xhr.timeout = opts.timeout
+  xhr.withCredentials = request.credentials === 'include'
 
   // Request
-  xhr.open(opts.method || 'GET', url, false)
+  xhr.open(request.method, request.url, false)
 
   try {
     xhr.responseType = 'arraybuffer'
@@ -19,98 +18,235 @@ function syncFetch (...args) {
     // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/responseType#Synchronous_XHR_restrictions
   }
 
-  for (const header of opts.headers) {
+  for (const header of request.headers) {
     xhr.setRequestHeader(...header)
   }
 
-  xhr.send(opts.body || null)
+  xhr.send(request.body || null)
 
   // Response
   let headers = xhr.getAllResponseHeaders()
   headers = headers && headers.split('\r\n').filter(Boolean).map(header => header.split(': ', 2))
 
-  return new syncFetch.Response(xhr.response, {
-    url: xhr.responseURL,
+  const response = new syncFetch.Response(xhr.response, {
+    headers,
     status: xhr.status,
     statusText: xhr.statusText,
-    headers,
-    redirected: xhr.responseURL !== url
   })
-}
 
-function parseArgs (resource, init) {
-  const request = []
+  response[INTERNALS].url = xhr.responseURL
+  response[INTERNALS].redirected = xhr.responseURL !== request.url
 
-  if (resource instanceof syncFetch.Request) {
-    request.push(resource.url)
-    request.push({
-      method: resource.method,
-      headers: resource.headers,
-      body: resource.body
-    })
-  } else {
-    request.push(resource, {})
-  }
-
-  Object.assign(request[1], init)
-
-  request[1].headers = new syncFetch.Headers(request[1].headers || {})
-
-  return request
+  return response
 }
 
 const INTERNALS = Symbol('SyncFetch Internals')
-
-class SyncRequest extends Request {
-  constructor (resource, init = {}, body = init.body) {
-    super(resource, init)
-    this[INTERNALS] = {
-      body: body ? Buffer.from(body) : null
-    }
-  }
-
-  clone () {
-    checkBody(this)
-    return new SyncRequest(this.url, this)
-  }
+const REQ_UNSUPPORTED = ['mode', 'cache', 'redirect', 'referrer', 'integrity']
+const HTTP_STATUS = {
+  '100': 'Continue',
+  '101': 'Switching Protocols',
+  '200': 'OK',
+  '201': 'Created',
+  '202': 'Accepted',
+  '203': 'Non-Authoritative Information',
+  '204': 'No Content',
+  '205': 'Reset Content',
+  '206': 'Partial Content',
+  '300': 'Multiple Choices',
+  '301': 'Moved Permanently',
+  '302': 'Found',
+  '303': 'See Other',
+  '304': 'Not Modified',
+  '305': 'Use Proxy',
+  '307': 'Temporary Redirect',
+  '400': 'Bad Request',
+  '401': 'Unauthorized',
+  '402': 'Payment Required',
+  '403': 'Forbidden',
+  '404': 'Not Found',
+  '405': 'Method Not Allowed',
+  '406': 'Not Acceptable',
+  '407': 'Proxy Authentication Required',
+  '408': 'Request Timeout',
+  '409': 'Conflict',
+  '410': 'Gone',
+  '411': 'Length Required',
+  '412': 'Precondition Failed',
+  '413': 'Payload Too Large',
+  '414': 'URI Too Long',
+  '415': 'Unsupported Media Type',
+  '416': 'Range Not Satisfiable',
+  '417': 'Expectation Failed',
+  '426': 'Upgrade Required',
+  '500': 'Internal Server Error',
+  '501': 'Not Implemented',
+  '502': 'Bad Gateway',
+  '503': 'Service Unavailable',
+  '504': 'Gateway Timeout',
+  '505': 'HTTP Version Not Supported'
 }
 
-class SyncResponse extends Response {
-  constructor (body, init = {}) {
-    body = body ? Buffer.from(body) : null
-    super(createStream(body), init)
-    this[INTERNALS] = {
-      url: init.url,
-      redirected: init.redirected,
-      body
+class SyncRequest {
+  constructor (resource, init = {}) {
+    for (const option of REQ_UNSUPPORTED) {
+      if (option in init) {
+        throw new TypeError(`option ${option} not supported`)
+      }
     }
+
+    if (init.credentials === 'same-origin') {
+      throw new TypeError('option credentials with value \'same-origin\' not supported')
+    }
+
+    this[INTERNALS] = {
+      method: init.method || 'GET',
+      headers: new syncFetch.Headers(init.headers),
+      body: init.body ? Buffer.from(init.body) : null,
+      credentials: init.credentials || 'omit'
+    }
+
+    if (typeof resource === 'string') {
+      this[INTERNALS].url = resource
+    } else if (resource instanceof SyncRequest) {
+      this[INTERNALS].url = resource.url
+      if (!init.method) {
+        this[INTERNALS].method = resource.method
+      }
+      if (!init.headers) {
+        this[INTERNALS].headers = resource.headers
+      }
+      if (!init.body) {
+        this[INTERNALS].body = resource[INTERNALS].body
+      }
+      if (!init.credentials) {
+        this[INTERNALS].credentials = resource.credentials
+      }
+    } else {
+      throw new TypeError('Request input should be a URL string or a Request object')
+    }
+  }
+
+  get cache () {
+    return 'default'
+  }
+
+  get credentials () {
+    return this[INTERNALS].credentials
+  }
+
+  get destination () {
+    return ''
+  }
+
+  get headers () {
+    return this[INTERNALS].headers
+  }
+
+  get integrity () {
+    return ''
+  }
+
+  get method () {
+    return this[INTERNALS].method
+  }
+
+  get mode () {
+    return 'cors'
+  }
+
+  get priority () {
+    return 'auto'
+  }
+
+  get redirect () {
+    return 'follow'
+  }
+
+  get referrer () {
+    return 'about:client'
+  }
+
+  get referrerPolicy () {
+    return ''
   }
 
   get url () {
     return this[INTERNALS].url
   }
 
+  clone () {
+    checkBody(this)
+    return new SyncRequest(this.url, this[INTERNALS])
+  }
+}
+
+class SyncResponse {
+  constructor (body, init = {}) {
+    this[INTERNALS] = {
+      body: body ? Buffer.from(body) : null,
+      bodyUsed: false,
+
+      headers: new syncFetch.Headers(init.headers),
+      status: init.status,
+      statusText: init.statusText,
+    }
+  }
+
+  get headers () {
+    return this[INTERNALS].headers
+  }
+
+  get ok () {
+    const status = this[INTERNALS].status
+    return status >= 200 && status < 300
+  }
+
   get redirected () {
     return this[INTERNALS].redirected
   }
 
+  get status () {
+    return this[INTERNALS].status
+  }
+
+  get statusText () {
+    return this[INTERNALS].statusText
+  }
+
+  get url () {
+    return this[INTERNALS].url
+  }
+
   clone () {
-    checkBody(this)
-    return new SyncResponse(this[INTERNALS].body, {
-      url: this.url,
-      headers: this.headers,
-      status: this.status,
-      statusText: this.statusText,
-      redirected: this.redirected
+    return this.redirect(this[INTERNALS].url, this[INTERNALS].status)
+  }
+
+  redirect (url, status) {
+    checkBody(body)
+
+    const response = new SyncResponse(this[INTERNALS].body, {
+      headers: this[INTERNALS].headers,
+      status: status || this[INTERNALS].status,
+      statusText: HTTP_STATUS[status] || this[INTERNALS].statusText
     })
+
+    response[INTERNALS].url = url || this[INTERNALS].url
+    response[INTERNALS].redirected = this[INTERNALS].redirected
+
+    return response
   }
 }
 
 class Body {
   constructor (body) {
     this[INTERNALS] = {
-      body: Buffer.from(body)
+      body: Buffer.from(body),
+      bodyUsed: false
     }
+  }
+
+  get bodyUsed () {
+    return this[INTERNALS].bodyUsed
   }
 
   static mixin (prototype) {
@@ -122,24 +258,20 @@ class Body {
   }
 
   arrayBuffer () {
-    checkBody(this)
     const buffer = consumeBody(this)
     return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength)
   }
 
   blob () {
-    checkBody(this)
     const type = this.headers && this.headers.get('content-type')
     return new Blob([consumeBody(this)], type && { type })
   }
 
   text () {
-    checkBody(this)
     return consumeBody(this).toString()
   }
 
   json () {
-    checkBody(this)
     try {
       return JSON.parse(consumeBody(this).toString())
     } catch (err) {
@@ -148,7 +280,6 @@ class Body {
   }
 
   buffer () {
-    checkBody(this)
     return consumeBody(this).clone()
   }
 }
@@ -160,27 +291,94 @@ function checkBody (body) {
 }
 
 function consumeBody (body) {
-  _super(body, 'arrayBuffer')()
+  checkBody(body)
+  body[INTERNALS].bodyUsed = true
   return body[INTERNALS].body || Buffer.alloc(0)
-}
-
-function _super (self, method) {
-  return Object.getPrototypeOf(Object.getPrototypeOf(self))[method].bind(self)
-}
-
-function createStream (body) {
-  return new ReadableStream({
-    start (controller) {
-      controller.enqueue(body)
-      controller.close()
-    }
-  })
 }
 
 Body.mixin(SyncRequest.prototype)
 Body.mixin(SyncResponse.prototype)
 
-syncFetch.Headers = self.Headers
+class Headers {
+  constructor (headers) {
+    if (headers instanceof syncFetch.Headers) {
+      this[INTERNALS] = { ...headers[INTERNALS] }
+    } else {
+      this[INTERNALS] = {}
+
+      if (Array.isArray(headers)) {
+        for (const [name, value] of headers) {
+          this.append(name, value)
+        }
+      } else if (typeof headers === 'object') {
+        for (const name in headers) {
+          this.set(name, headers[name])
+        }
+      }
+    }
+  }
+
+  // modification
+  append (name, value) {
+    name = name.toLowerCase()
+    if (!this[INTERNALS][name]) {
+      this[INTERNALS][name] = []
+    }
+    this[INTERNALS][name].push(value)
+  }
+
+  delete (name) {
+    delete this[INTERNALS][name.toLowerCase()]
+  }
+
+  set (name, value) {
+    this[INTERNALS][name.toLowerCase()] = [value]
+  }
+
+  // access
+  entries () {
+    const pairs = []
+    for (const name in this[INTERNALS]) {
+      for (const value of this[INTERNALS][name]) {
+        pairs.push([name, value])
+      }
+    }
+    return pairs
+  }
+
+  get (name) {
+    name = name.toLowerCase()
+    return name in this[INTERNALS] ? this[INTERNALS][name].join(', ') : null
+  }
+
+  keys () {
+    return Object.keys(this[INTERNALS])
+  }
+
+  has (name) {
+    return name.toLowerCase() in this[INTERNALS]
+  }
+
+  values () {
+    const values = []
+    for (const name in this[INTERNALS]) {
+      for (const value of this[INTERNALS][name]) {
+        values.push(value)
+      }
+    }
+    return values
+  }
+
+  *[Symbol.iterator] () {
+    for (const name in this[INTERNALS]) {
+      for (const value of this[INTERNALS][name]) {
+        yield [name, value]
+      }
+    }
+  }
+}
+
+syncFetch.Headers = Headers
 syncFetch.Request = SyncRequest
 syncFetch.Response = SyncResponse
 module.exports = syncFetch

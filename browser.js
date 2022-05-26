14ab20b0208c06ebb1a482b31604568a3c3a3137
Lars Willighagen <lars.willighagen@gmail.com>
2022-05-26 22:20:46 +0200

    style(browser): apply lint fixes

--- browser.js
+++ browser.js
@@ -31,7 +31,7 @@ function syncFetch (...args) {
   const response = new syncFetch.Response(xhr.response, {
     headers,
     status: xhr.status,
-    statusText: xhr.statusText,
+    statusText: xhr.statusText
   })
 
   response[INTERNALS].url = xhr.responseURL
@@ -43,47 +43,47 @@ function syncFetch (...args) {
 const INTERNALS = Symbol('SyncFetch Internals')
 const REQ_UNSUPPORTED = ['mode', 'cache', 'redirect', 'referrer', 'integrity']
 const HTTP_STATUS = {
-  '100': 'Continue',
-  '101': 'Switching Protocols',
-  '200': 'OK',
-  '201': 'Created',
-  '202': 'Accepted',
-  '203': 'Non-Authoritative Information',
-  '204': 'No Content',
-  '205': 'Reset Content',
-  '206': 'Partial Content',
-  '300': 'Multiple Choices',
-  '301': 'Moved Permanently',
-  '302': 'Found',
-  '303': 'See Other',
-  '304': 'Not Modified',
-  '305': 'Use Proxy',
-  '307': 'Temporary Redirect',
-  '400': 'Bad Request',
-  '401': 'Unauthorized',
-  '402': 'Payment Required',
-  '403': 'Forbidden',
-  '404': 'Not Found',
-  '405': 'Method Not Allowed',
-  '406': 'Not Acceptable',
-  '407': 'Proxy Authentication Required',
-  '408': 'Request Timeout',
-  '409': 'Conflict',
-  '410': 'Gone',
-  '411': 'Length Required',
-  '412': 'Precondition Failed',
-  '413': 'Payload Too Large',
-  '414': 'URI Too Long',
-  '415': 'Unsupported Media Type',
-  '416': 'Range Not Satisfiable',
-  '417': 'Expectation Failed',
-  '426': 'Upgrade Required',
-  '500': 'Internal Server Error',
-  '501': 'Not Implemented',
-  '502': 'Bad Gateway',
-  '503': 'Service Unavailable',
-  '504': 'Gateway Timeout',
-  '505': 'HTTP Version Not Supported'
+  100: 'Continue',
+  101: 'Switching Protocols',
+  200: 'OK',
+  201: 'Created',
+  202: 'Accepted',
+  203: 'Non-Authoritative Information',
+  204: 'No Content',
+  205: 'Reset Content',
+  206: 'Partial Content',
+  300: 'Multiple Choices',
+  301: 'Moved Permanently',
+  302: 'Found',
+  303: 'See Other',
+  304: 'Not Modified',
+  305: 'Use Proxy',
+  307: 'Temporary Redirect',
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  402: 'Payment Required',
+  403: 'Forbidden',
+  404: 'Not Found',
+  405: 'Method Not Allowed',
+  406: 'Not Acceptable',
+  407: 'Proxy Authentication Required',
+  408: 'Request Timeout',
+  409: 'Conflict',
+  410: 'Gone',
+  411: 'Length Required',
+  412: 'Precondition Failed',
+  413: 'Payload Too Large',
+  414: 'URI Too Long',
+  415: 'Unsupported Media Type',
+  416: 'Range Not Satisfiable',
+  417: 'Expectation Failed',
+  426: 'Upgrade Required',
+  500: 'Internal Server Error',
+  501: 'Not Implemented',
+  502: 'Bad Gateway',
+  503: 'Service Unavailable',
+  504: 'Gateway Timeout',
+  505: 'HTTP Version Not Supported'
 }
 
 class SyncRequest {
@@ -188,7 +188,7 @@ class SyncResponse {
 
       headers: new syncFetch.Headers(init.headers),
       status: init.status,
-      statusText: init.statusText,
+      statusText: init.statusText
     }
   }
 
@@ -222,7 +222,7 @@ class SyncResponse {
   }
 
   redirect (url, status) {
-    checkBody(body)
+    checkBody(this)
 
     const response = new SyncResponse(this[INTERNALS].body, {
       headers: this[INTERNALS].headers,
@@ -369,7 +369,7 @@ class Headers {
     return values
   }
 
-  *[Symbol.iterator] () {
+  * [Symbol.iterator] () {
     for (const name in this[INTERNALS]) {
       for (const value of this[INTERNALS][name]) {
         yield [name, value]
